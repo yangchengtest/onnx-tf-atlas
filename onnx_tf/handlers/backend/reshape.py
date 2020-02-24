651a7e69896f5a7a59d598c8bb4182c84db7a6a8
yangchengtest <yangcheng830117@163.com>
2020-02-24 18:32:46 +0800

    RESHAPE简化

--- onnx_tf/handlers/backend/reshape.py
+++ onnx_tf/handlers/backend/reshape.py
@@ -5,7 +5,7 @@ import tensorflow as tf
 from onnx_tf.handlers.backend_handler import BackendHandler
 from onnx_tf.handlers.handler import onnx_op
 from onnx_tf.handlers.handler import tf_func
-
+import numpy as np
 
 @onnx_op("Reshape")
 @tf_func(tf.reshape)
@@ -14,24 +14,33 @@ class Reshape(BackendHandler):
   @classmethod
   def _common(cls, node, **kwargs):
     tensor = kwargs["tensor_dict"][node.inputs[0]]
+    is_const = False
     if cls.SINCE_VERSION == 1:
       shape = tf.constant(node.attrs["shape"], dtype=tf.int64)
     else:  # since_version >= 5
       shape = tf.cast(kwargs["tensor_dict"][node.inputs[1]], tf.int64)
+      is_const = True
     input_shape = tf.shape(tensor, out_type=tf.int64)
-
+    if is_const:
+      sess = tf.Session()
+      with sess.as_default():
+        shape_val = shape.eval().astype(np.int64)
+        print ("shape_val",shape_val)  
+        copied_shape = shape_val
+    else:
     # Extract indicies of the shape paramter where
     # a copy from the original dimension size is needed.
-    copy_indices = tf.squeeze(
-        tf.where(tf.equal(shape, tf.constant(0, dtype=tf.int64))), -1)
-
-    indices_gathered = tf.gather(input_shape, copy_indices)
-    indices_scattered = tf.sparse_to_dense(copy_indices,
+      copy_indices = tf.squeeze(
+          tf.where(tf.equal(shape, tf.constant(0, dtype=tf.int64))), -1)
+    
+      indices_gathered = tf.gather(input_shape, copy_indices)
+      indices_scattered = tf.sparse_to_dense(copy_indices,
                                            tf.cast(tf.shape(shape), tf.int64),
                                            indices_gathered)
 
     # Perform the copy wherever requested (wherever dim_size == 0)
-    copied_shape = shape + indices_scattered
+      copied_shape = shape + indices_scattered
+      
     attrs = copy.deepcopy(node.attrs)
     attrs.pop("shape", None)
     return [
